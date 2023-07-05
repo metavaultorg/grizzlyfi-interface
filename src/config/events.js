41c93d3ee19aaad4ecd31fe7894040a1d236a1fe
xNx <hx@metadesk.tech>
2023-07-05 21:51:39 +0800

    correct some text

--- src/config/events.js
+++ src/config/events.js
@@ -1,7 +1,7 @@
 let eventsData = [
   {
     id: "metavault-trade-verified",
-    title: "Metavault Official Domains",
+    title: "Grizzly Trade Official Domains",
     isActive: true,
     validTill: "02/01/2023 12:00:00 PM",
     bodyText: "Please verify that you are on the correct domain. https://trade.grizzly.fi/#/trade",
@@ -18,13 +18,13 @@ let eventsData = [
   },
   {
     id: "metavault-trade-launch-2",
-    title: "Metavault OrderBook Update",
+    title: "Grizzly Trade OrderBook Update",
     isActive: false,
     validTill: "03/30/2023 12:00:00 PM",
     bodyText: "We have updated the orderbook contract, we ask you to cancel old orders (limit, SL, TP) via the link below and place them again on the regular dApp.",
     buttons: [
       {
-        text: "https://cancel-order.grizzly.trade",
+        text: "https://trade.grizzly.trade",
         link: "https://trade.grizzly.fi/#/trade",
       }
     ],
