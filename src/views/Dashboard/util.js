60bdbcf3c90b9c76314055c0e5e478ce62d0abba
yalingF <lynnf@metadesk.tech>
2023-07-25 15:40:59 +0800

    fix table sort issue

--- src/views/Dashboard/util.js
+++ src/views/Dashboard/util.js
@@ -1,11 +1,10 @@
 export function sortArr(arr, key, isAsc) {
     let result = arr.sort(function add(a, b) {
         if (isAsc) {
-            return a[key] - b[key]
+            return parseFloat(a[key]) - parseFloat(b[key])
         } else { //desc
-            return b[key] - a[key]
+            return parseFloat(b[key]) - parseFloat(a[key])
         }
     })
-    // console.log(result);w
     return result
 }
